91572ff4fd4517aaace9960b8ebbf310603a2185
wasehabbasi <abdulwasehabbasi@hotmail.com>
2026-08-26 00:40:46 +0500

    test

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { logger } from "./lib/logger";
 
-const rawPort = process.env["PORT"];
+const rawPort = process.env["PORT"] || 3000;
 
 if (!rawPort) {
   throw new Error(
